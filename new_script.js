07bdfb322df963cb511eba62fff3ae96db7638ad
Shahmeer <shahmeer.a.khan@gmail.com>
2019-07-14 01:46:17 +0500

    Completed Project

--- new_script.js
+++ new_script.js
@@ -8,10 +8,10 @@ function makeGrid(gridSize){
     let totalborder =  4 * gridSize;
     container.style.width = 480 + totalborder + "px";
     for (let i = 0; i < gridSize; i++){
-        let colBlock = document.createElement('div');
+        const colBlock = document.createElement('div');
         colBlock.classList.add('block');
         for (let j = 0; j < gridSize; j++){
-            let rowBlock = document.createElement('div');
+            const rowBlock = document.createElement('div');
             rowBlock.style.borderWidth = "2px";
             rowBlock.style.borderStyle = "solid";
             rowBlock.style.float = "left";
@@ -28,6 +28,18 @@ function makeGrid(gridSize){
 }
 
 function reset(){
-    
+    //const container = document.getElementById("#containerGrid");
+    let child = container.lastElementChild
+    while(child){
+      container.removeChild(child);
+      child = container.lastElementChild;
+    }
+    makeGrid(gridSize);
+}
+
+function generate(){
+    var mainSize = document.getElementById("sizeInput").value;
+    gridSize = mainSize;
+    reset();
 }
 
